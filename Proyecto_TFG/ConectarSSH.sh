2a4c6e067370a97f0cba4f7ed194978e460fdb88
masanmas <masanmasupv@gmail.com>
2021-04-11 18:43:07 +0200

    Test GPUs

---
+++ Proyecto_TFG/ConectarSSH.sh
@@ -0,0 +1,11 @@
+#GPUs: 24 and 17
+read "Num_MV"
+
+
+NAME="msanchez@prhltgpu"$Num_MV".prhlt.upv.es"
+
+echo $NAME
+
+ssh $NAME
+
+
---
+++ Proyecto_TFG/Git_Scripts/PushRequest.sh
@@ -0,0 +1,7 @@
+git remote add GPUs https://github.com/masanmas/GPUs
+
+git add .
+git commit
+git push GPUs master
+
+
